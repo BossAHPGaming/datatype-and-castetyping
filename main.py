6a2bf4b48a5e43ee201d5d5afec8d7248b74aea5
BossAHPGaming <aaravhpatel210@gmail.com>
2024-11-25 18:19:58 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,21 @@
+name = "Aarav"
+age = 17
+is_student = 12
+weight = 65.8
+
+print("name :", name)
+print("Data Type of name is ", type(name))
+print("age :", age)
+print("Data Type of age is ", type(age))
+print("is_student :", is_student)
+print("Data Type of is_student is ", type(is_student))
+print("weight :", weight)
+print("Data Type of weight is ", type(weight))
+
+print("\n After Type Casting...")
+age = str(age)
+print(age)
+print("Datatype of age is ", type(age))
+weight = int(weight)
+print(weight)
+print("Datatype of weight is ", type(weight))
